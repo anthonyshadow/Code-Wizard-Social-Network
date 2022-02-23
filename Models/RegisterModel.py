c73c1c3f8f3670da0c227bdc1365ba184c38bb6b
anthonyshadow <anthony.shadowitz@gmail.com>
2022-02-23 15:26:04 -0500

    hash passwords

--- Models/RegisterModel.py
+++ Models/RegisterModel.py
@@ -1,5 +1,6 @@
 import pymongo
 from pymongo import MongoClient
+import bcrypt
 
 class RegisterModel:
     def __init__(self):
@@ -8,6 +9,12 @@ class RegisterModel:
         self.Users = self.db.users
 
     def insert_user(self, data):
+        hashed = bcrypt.hashpw(data.password.encode(), bcrypt.gensalt)
 
-        id = self.users.insert({"username": data.username, "name": data.name, "password": data.password, "email": data.email})
+        id = self.users.insert({"username": data.username, "name": data.name, "password": hashed, "email": data.email})
         print("uid is", id)
+
+        myuser = self.Users.find_one({"username": data.username})
+
+        if bcrypt.checkpw("avocado1".encode(), myuser["password"]):
+            print("this matches")
